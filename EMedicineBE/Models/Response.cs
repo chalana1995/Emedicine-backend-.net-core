716cb176d2db44f23df3eaba6c5c3049309ac602
chalana kalpitha <chalanakalpitha72@gmail.com>
2022-12-19 19:35:12 +0530

    set up back end

---
+++ EMedicineBE/Models/Response.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EMedicineBE.Models
+{
+    public class Response
+    {
+        public int StatusCode { get; set; }
+
+        public string StatusMessage { get; set; }
+    }
+}
