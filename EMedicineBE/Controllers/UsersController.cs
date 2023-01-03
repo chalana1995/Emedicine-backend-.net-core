38810d6282690ef04609397ad5027283451f3cfa
chalana kalpitha <chalanakalpitha72@gmail.com>
2023-01-03 20:32:57 +0530

    add package

---
+++ EMedicineBE/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using EMedicineBE.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EMedicineBE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public UsersController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        [Route("registration")]
+        public Response register(Users users)
+        {
+            Response response = new Response();
+            SqlConnection connection = new SqlConnection(_configuration.GetConnectionString("EMedCS").ToString());
+
+            return response;
+        }
+    }
+}
